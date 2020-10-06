983ed23c1a681d296baad1c24f0804e3710aab5c
Niels de Vries <niels@qup.eu>
2020-10-06 15:11:31 +0200

    fixed tests, 100% coverage

--- envvar/env_struct_test.go
+++ envvar/env_struct_test.go
@@ -6,18 +6,48 @@ import (
 	"testing"
 )
 
+type NotStruct bool
+
 type ExampleStruct struct {
-	Foo string `env:"foo,required"`
-	Bar string
+	Foo   string `env:"foo,required"`
+	Bar   string
+	Unset string `env:"not-here"`
 }
 
 type InvalidTypeExampleStruct struct {
 	Foo int `env:"foo,required"`
 }
 
+type UnexportedVarExampleStruct struct {
+	bar string `env:"bar"`
+	foo string `env:"foo,required"`
+}
+
+func TestToStructWithNoStructFails(t *testing.T) {
+	test := NotStruct(false)
+
+	err := ToStruct(&test)
+
+	if err == nil {
+		t.Fail()
+	}
+}
+
 func TestToStructWithMissingRequiredEnvVar(t *testing.T) {
 	os.Unsetenv("foo")
 	testStruct := ExampleStruct{}
+
+	err := ToStruct(&testStruct)
+
+	if err == nil {
+		t.Fail()
+	}
+}
+
+func TestToStructWithUnexportedVarFails(t *testing.T) {
+	os.Setenv("bar", "hi")
+	os.Setenv("foo", "hi")
+	testStruct := UnexportedVarExampleStruct{}
 
 	err := ToStruct(&testStruct)
 
