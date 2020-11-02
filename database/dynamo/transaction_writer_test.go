9824bdfa9706f2597604351e6c8fce4b27d930c6
Niels de Vries <niels@qup.eu>
2020-11-02 11:17:03 +0100

    Enough initial coverage for release

---
+++ database/dynamo/transaction_writer_test.go
@@ -0,0 +1,86 @@
+package dynamo
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestTransactionWriter_Delete(t *testing.T) {
+	connection := TestConnection{}
+	dynamoClient := CreateNewQupDynamo(connection)
+
+	transaction, err := dynamoClient.Transaction("mockTable", ExampleRecord{})
+
+	if err != nil {
+		t.Fail()
+	}
+
+	newTransaction := transaction.Delete("test123")
+	if *newTransaction.TransactionQuery.TransactItems[0].Delete.Key["id"].S != "test123" {
+		t.Fail()
+	}
+}
+
+func TestTransactionWriter_Save(t *testing.T) {
+	connection := TestConnection{}
+	dynamoClient := CreateNewQupDynamo(connection)
+
+	transaction, err := dynamoClient.Transaction("mockTable", ExampleRecord{})
+
+	if err != nil {
+		t.Fail()
+	}
+
+	newTransaction := transaction.Save(&ExampleRecord{Id: "test456"})
+	if *newTransaction.TransactionQuery.TransactItems[0].Put.Item["id"].S != "test456" {
+		t.Fail()
+	}
+}
+
+func TestTransactionWriter_Commit(t *testing.T) {
+	connection := TestConnection{
+		MockTransactError: nil,
+	}
+	dynamoClient := CreateNewQupDynamo(connection)
+
+	transaction, err := dynamoClient.Transaction("mockTable", ExampleRecord{})
+
+	if err != nil {
+		t.Fail()
+	}
+
+	errs := transaction.Commit()
+
+	if errs != nil {
+		t.Fail()
+	}
+}
+
+func TestTransactionWriter_CommitFail(t *testing.T) {
+	connection := TestConnection{
+		MockTransactError: fmt.Errorf("transact fail"),
+	}
+	dynamoClient := CreateNewQupDynamo(connection)
+
+	transaction, err := dynamoClient.Transaction("mockTable", ExampleRecord{})
+
+	if err != nil {
+		t.Fail()
+	}
+
+	errs := transaction.Commit()
+
+	if errs == nil {
+		t.Fail()
+	}
+
+	listErrors := *errs
+
+	if len(listErrors) != 1 {
+		t.Fail()
+	}
+
+	if listErrors[0].Error() != "transact fail" {
+		t.Fail()
+	}
+}
